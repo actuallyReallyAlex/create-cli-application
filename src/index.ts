5fbd4ccff477cbe90d349c7efb3bfb673edf32b5
alexlee-dev <alex@alexlee.dev>
2020-05-25 13:52:20 -0700

    ✏️ Yargs POC

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,25 @@
+import yargs from "yargs";
+
 const main = () => {
-  console.log("Hello World");
+  yargs
+    .scriptName("create-cli-application")
+    .usage("$0 [application-name]")
+    .command(
+      "create",
+      "Creates a bootstrapped cli application in the current directory.",
+      (yargs) => {
+        yargs.positional("application-name", {
+          type: "string",
+          default: "cool-app",
+          describe: "The name of the application to create.",
+        });
+      },
+      function (argv) {
+        console.log({ argv });
+      }
+    )
+    .demandCommand()
+    .help().argv;
 };
 
 export default main;
