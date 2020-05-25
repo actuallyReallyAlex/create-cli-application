195a923d8a1d8c719c2ea2a5c299526846419332
alexlee-dev <alex@alexlee.dev>
2020-05-25 14:43:45 -0700

    ✏️ Create package.json in application directory

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,46 @@
 import chalk from "chalk";
 import commander from "commander";
-// import path from "path";
+import fs from "fs-extra";
+import os from "os";
+import path from "path";
 
-const main = () => {
-  let projectName;
+const createProjectDirectory = async (
+  applicationName: string
+): Promise<void> => {
+  const root = path.resolve(applicationName);
+  const originalDirectory = process.cwd();
+
+  fs.ensureDirSync(root);
+
+  console.log();
+  console.log(`Creating a new React app in ${chalk.yellowBright(root)}.`);
+  console.log();
+
+  const packageJson = {
+    name: applicationName,
+    version: "0.0.0",
+  };
+
+  try {
+    await fs.writeFile(
+      path.join(root, "package.json"),
+      JSON.stringify(packageJson, null, 2) + os.EOL
+    );
+
+    console.log("Installing packages...");
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const main = async (): Promise<void> => {
+  let applicationName;
   const program = new commander.Command("create-cli-application")
     .version("0.0.0")
     .arguments("<application-name>")
     .usage(`${chalk.yellowBright("<application-name>")} [options]`)
     .action((name) => {
-      projectName = name;
+      applicationName = name;
     })
     .on("--help", () => {
       console.log(
@@ -24,7 +55,7 @@ const main = () => {
     })
     .parse(process.argv);
 
-  if (projectName === "." || !projectName) {
+  if (applicationName === "." || !applicationName) {
     console.error("\nPlease specify the name of your application:");
     console.log(
       `\t${chalk.cyan(program.name())} ${chalk.green("<application-name>")}\n`
@@ -41,7 +72,7 @@ const main = () => {
 
   // TODO - Catch names like "my.app.name" or "my app name"
 
-  console.log({ projectName });
+  await createProjectDirectory(applicationName);
 };
 
 export default main;
