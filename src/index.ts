bc1249c9c227387c2d076c993a33639bc4b8f66a
alexlee-dev <alex@alexlee.dev>
2020-05-25 14:20:25 -0700

    ✏️ Switch to Commander

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,47 @@
-import yargs from "yargs";
+import chalk from "chalk";
+import commander from "commander";
+// import path from "path";
 
 const main = () => {
-  yargs
-    .scriptName("create-cli-application")
-    .usage("$0 [application-name]")
-    .command(
-      "create",
-      "Creates a bootstrapped cli application in the current directory.",
-      (yargs) => {
-        yargs.positional("application-name", {
-          type: "string",
-          default: "cool-app",
-          describe: "The name of the application to create.",
-        });
-      },
-      function (argv) {
-        console.log({ argv });
-      }
-    )
-    .demandCommand()
-    .help().argv;
+  let projectName;
+  const program = new commander.Command("create-cli-application")
+    .version("0.0.0")
+    .arguments("<application-name>")
+    .usage(`${chalk.yellowBright("<application-name>")} [options]`)
+    .action((name) => {
+      projectName = name;
+    })
+    .on("--help", () => {
+      console.log(
+        `\nOnly ${chalk.yellowBright("<application-name>")} is required.`
+      );
+      console.log(`\nIf you run into a problem, please open up a new issue:`);
+      console.log(
+        `${chalk.cyan(
+          "https://github.com/alexlee-dev/create-cli-application/issues/new"
+        )}\n`
+      );
+    })
+    .parse(process.argv);
+
+  if (projectName === "." || !projectName) {
+    console.error("\nPlease specify the name of your application:");
+    console.log(
+      `\t${chalk.cyan(program.name())} ${chalk.green("<application-name>")}\n`
+    );
+    console.log("For example:");
+    console.log(
+      `  ${chalk.cyan(program.name())} ${chalk.green("my-cool-app")}\n`
+    );
+    console.log(
+      `Run ${chalk.cyan(`${program.name()} --help`)} to see all options.`
+    );
+    process.exit(1);
+  }
+
+  // TODO - Catch names like "my.app.name" or "my app name"
+
+  console.log({ projectName });
 };
 
 export default main;
