50b699f20cd4a63858e9325230b7069d1ae73540
alexlee-dev <alex@alexlee.dev>
2020-06-02 13:02:49 -0700

    ✨ Add Favicon

--- website/src/server/controllers/assets.ts
+++ website/src/server/controllers/assets.ts
@@ -5,11 +5,21 @@ class AssetsController {
   public router: Router = express.Router();
 
   static assetList: string[] = [
+    'android-chrome-192x192.png',
+    'android-chrome-512x512.png',
+    'apple-touch-icon.png',
     'background.png',
+    'browserconfig.xml',
+    'favicon-16x16.png',
+    'favicon-32x32.png',
+    'favicon.ico',
     'gdemo.min.css',
     'milligram.css',
     'milligram.min.css.map',
+    'mstile-150x150.png',
     'normalize.css',
+    'safar-pinned-tab.svg',
+    'site.webmanifest',
   ];
 
   constructor() {
