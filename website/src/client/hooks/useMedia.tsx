bc6cbb0b58c0594b58f1342c8d9ea9f2543ace34
alexlee-dev <alex@alexlee.dev>
2020-06-02 15:19:38 -0700

    ✏️ Add return types on useMedia

--- website/src/client/hooks/useMedia.tsx
+++ website/src/client/hooks/useMedia.tsx
@@ -15,7 +15,7 @@ const useMedia = (
   const mediaQueryLists = queries.map((q) => window.matchMedia(q));
 
   // Function that gets value based on matching media query
-  const getValue = () => {
+  const getValue = (): any => {
     // Get index of first media query that matches
     const index = mediaQueryLists.findIndex((mql) => mql.matches);
     // Return related value or defaultValue if none
@@ -30,11 +30,11 @@ const useMedia = (
       // Event listener callback
       // Note: By defining getValue outside of useEffect we ensure that it has ...
       // ... current values of hook args (as this hook callback is created once on mount).
-      const handler = () => setValue(getValue);
+      const handler = (): void => setValue(getValue);
       // Set a listener for each media query with above handler as callback.
       mediaQueryLists.forEach((mql) => mql.addListener(handler));
       // Remove listeners on cleanup
-      return () =>
+      return (): void =>
         mediaQueryLists.forEach((mql) => mql.removeListener(handler));
     },
     [], // Empty array ensures effect is only run on mount and unmount
